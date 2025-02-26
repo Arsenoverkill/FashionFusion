0ed0a2cf38c8da519ad6dd352d983bd0bf52633a
Arsenoverkill <arsenprogrammer007@gmail.com>
2025-02-26 17:09:01 +0600

    prisma

---
+++ server/src/clothes/type.d.ts
@@ -0,0 +1,15 @@
+interface IClothes {
+  _id?: number;
+  title: string;
+  price: number;
+  image: string;
+  imageHover: string;
+  description: string;
+  quintity: number;
+  quantity: number;
+  size: number[];
+  sizeProduct: number;
+  category: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
